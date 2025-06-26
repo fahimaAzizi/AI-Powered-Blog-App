3b9ed0a64fb3c57f9fd04e30c1ca2569631e45cf
Fahima Azizi <fahimaazizi397@gmail.com>
2025-06-26 21:31:14 +0500

    update

---
+++ client/src/assets/assets.js
@@ -0,0 +1,77 @@
+// Blog Pictures
+import blog_pic_1 from './blog_pic_1.png';
+import blog_pic_2 from './blog_pic_2.png';
+import blog_pic_3 from './blog_pic_3.png';
+import blog_pic_4 from './blog_pic_4.png';
+import blog_pic_5 from './blog_pic_5.png';
+import blog_pic_6 from './blog_pic_6.png';
+import blog_pic_7 from './blog_pic_7.png';
+import blog_pic_8 from './blog_pic_8.png';
+import blog_pic_9 from './blog_pic_9.png';
+import blog_pic_10 from './blog_pic_10.png';
+
+// Social Media Icons
+import facebook_icon from './facebook_icon.svg';
+import googleplus_icon from './googleplus_icon.svg';
+import twitter_icon from './twitter_icon.svg';
+
+// Logos and UI Elements
+import logo from './logo.svg';
+import arrow from './arrow.svg';
+import logo_light from './logo_light.svg';
+import blog_icon from './blog_icon.png';
+import add_icon from './add_icon.svg';
+import list_icon from './list_icon.svg';
+import email_icon from './email_icon.png';
+import upload_area from './upload_area.svg';
+import user_icon from './user_icon.svg';
+import bin_icon from './bin_icon.svg';
+import comment_icon from './comment_icon.svg';
+import tick_icon from './tick_icon.svg';
+import star_icon from './star_icon.svg';
+import cross_icon from './cross_icon.svg';
+import home_icon from './home_icon.svg';
+import gradientBackground from './gradientBackground.png';
+
+// Dashboard Icons
+import dashboard_icon_1 from './dashboard_icon_1.svg';
+import dashboard_icon_2 from './dashboard_icon_2.svg';
+import dashboard_icon_3 from './dashboard_icon_3.svg';
+import dashboard_icon_4 from './dashboard_icon_4.svg';
+
+// Export all assets as a single object
+export const assets = {
+  blog_pic_1,
+  blog_pic_2,
+  blog_pic_3,
+  blog_pic_4,
+  blog_pic_5,
+  blog_pic_6,
+  blog_pic_7,
+  blog_pic_8,
+  blog_pic_9,
+  blog_pic_10,
+  facebook_icon,
+  googleplus_icon,
+  twitter_icon,
+  logo,
+  arrow,
+  logo_light,
+  blog_icon,
+  add_icon,
+  list_icon,
+  email_icon,
+  upload_area,
+  user_icon,
+  bin_icon,
+  comment_icon,
+  tick_icon,
+  star_icon,
+  cross_icon,
+  home_icon,
+  gradientBackground,
+  dashboard_icon_1,
+  dashboard_icon_2,
+  dashboard_icon_3,
+  dashboard_icon_4
+};
