66f43c8f8d58290fb7ed8a2b65cee1756b01c2e4
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-03 16:56:34 +0500

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Form, Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react';
+import {useClerk ,UserButton ,useUser} from '@clerk/clerk-react'
 
-const Navbar = () => {
+const  Navbar = () => {
+  
+  const [isOpen , setIsOpen] = useState(false)
+  const {user} =useUser()
+  const{openSignIn} =useClerk()
+
+
+
+
+
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
       <Link to='/' className="max-md:flex-1">
@@ -21,12 +31,21 @@ const Navbar = () => {
 
       <div>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
+        {
+          !user ?(
+             <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2
+              bg-primary hover:bg-primary-dull transition rounded-full 
+              font-medium cursor-pointer'>
           Login
         </button>
+          ) :(
+            <UserButton/>
+          )
+        }
+       
       </div>
 
-      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' />
+      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
     </div>
   );
 };
