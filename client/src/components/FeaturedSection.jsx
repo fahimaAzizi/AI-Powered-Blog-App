fdc550fde3ff46543c6aa42dded1941c66e67c67
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-07 12:18:42 +0500

    update

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,14 +1,14 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom' 
 
 const FeaturedSection = () => {
   const navigate = useNavigate()
 
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
-      <div>
-        <p>Now Showing</p>
+      <div className='relative flex items-center justify-between pt-20 pb-10'>
+        <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
         <button 
           onClick={() => navigate('/movies')}
           className='group flex items-center gap-2 text-sm text-gray-300'
@@ -17,6 +17,7 @@ const FeaturedSection = () => {
           <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5' />
         </button>
       </div>
+      <div></div>
     </div>
   )
 }
