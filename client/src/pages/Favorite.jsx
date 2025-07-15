59c7ea10593725107fee121a9f3f119a0b73edce
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-15 11:55:52 +0500

    update

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -3,13 +3,13 @@ import { dummyShowsData } from '../assets/assets'
 import MovieCard from '../components/MovieCard'
 import BlurCircle from '../components/BlurCircle'
 
-const Movies = () => {
+const Favorite = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <BlurCircle top="150px" left="0px" />
       <BlurCircle bottom="50px" right="50px" />
 
-      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <h1 className='text-lg font-medium my-4'>Your Favorite Movies</h1>
 
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map((movie) => (
@@ -22,4 +22,4 @@ const Movies = () => {
   )
 }
 
-export default Movies
+export default Favorite
--- client/src/pages/MoviesDetails.jsx
+++ client/src/pages/MoviesDetails.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const MoviesDetails = () => {
+  const {id} =useParams()
+  const [Show ,setShow] =useState(null)
+  const getShow = async ()=>{
+    
+  }
   return (
-    <div>MoviesDetails</div>
+    <div>
+
+    </div>
   )
 }
 
