648bb1922d918f5429cae808d0cbf8d58272a9f6
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-15 18:03:37 +0500

    update

--- client/src/pages/MoviesDetails.jsx
+++ client/src/pages/MoviesDetails.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
+import {dummyDateTimeData, dummyShowsData} from '../assets/assets'
 const MoviesDetails = () => {
   const {id} =useParams()
   const [Show ,setShow] =useState(null)
   const getShow = async ()=>{
-    
+    const show = dummyShowsData.find(Show=> Show._id ===id)
+    setShow({
+      moive :show,
+      dataTime: dummyDateTimeData
+    })
   }
+
+  useEffect(()=>{
+    getShow()
+  },(id))
   return (
     <div>
 
