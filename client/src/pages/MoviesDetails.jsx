0083fbb1953c743b7d2fe2764f2753d8be6cabdb
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-17 12:01:30 +0500

    update

--- client/src/pages/MoviesDetails.jsx
+++ client/src/pages/MoviesDetails.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {dummyDateTimeData, dummyShowsData} from '../assets/assets'
+import BlurCircle from '../components/BlurCircle'
+import {Heart ,StarIcon} from 'lucide-react'
+import timeFormat from '../lib/timeFormat'
+
 const MoviesDetails = () => {
   const {id} =useParams()
-  const [Show ,setShow] =useState(null)
+  const [show ,setShow] =useState(null)
   const getShow = async ()=>{
-    const Show = dummyShowsData.find(Show=> Show._id ===id)
+    const show = dummyShowsData.find(show=> show._id ===id)
     setShow({
-      moive :Show,
+      moive :show,
       dataTime: dummyDateTimeData
     })
   }
@@ -15,22 +19,30 @@ const MoviesDetails = () => {
   useEffect(()=>{
     getShow()
   },[id])
-  return Show ?(
+  return show ?(
     <div className=' px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
        <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
-        <img src={Show.moive.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
+        <img src={show.moive.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
         <div className='relative flex flex-col gap-3'>
           <BlurCircle top=" -100px" left="-100px"/>
           <p className='text-primary'>ENGLISH</p>
-          <h1 className='text-4xl font-semibold max-w-69 text-blance'>{Show.movie.title}</h1>
+          <h1 className='text-4xl font-semibold max-w-69 text-blance'>{show.movie.title}</h1>
           <div className='flex items-cener gap-2 text-gray-300'>
-            <StarIcon className='w-5 h-5 text-primary fill-primary'/>
-            {Show.movie.vote_average.toFixed(1)}Uaer Rathing
+           <StarIcon className='w-5 h-5 text-primary fill-primary'/>
+          
+            {show.movie.vote_average.toFixed(1)}Uaer Rathing
           </div>
           <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{
-            Show.movie.overview}
+            show.movie.overview}
             </p>
-            <p >{timeFormat(Show.moive.runtime)} . {Show.movie.genrs.map(genre=>genre.name).join(",")} . {Show.moive.release_date.splt("-")[0]}</p>
+            <p >{timeFormat(show.moive.runtime)} . {show.movie.genrs.map(genre=>genre.name).join(",")} . {show.moive.release_date.splt("-")[0]}</p>
+            <div>
+              <button>
+                <button>Watch Trailer</button>
+                <a href=""> Buy Tickets</a>
+                <Heart className={`w-5 h-5`}/>
+              </button>
+            </div>
         </div>
        </div>
     </div>
