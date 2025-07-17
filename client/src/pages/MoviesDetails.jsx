a2003aed3da472b269c974b66f24cb59ccf3adb1
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-17 11:28:19 +0500

    update

--- client/src/pages/MoviesDetails.jsx
+++ client/src/pages/MoviesDetails.jsx
@@ -19,7 +19,19 @@ const MoviesDetails = () => {
     <div className=' px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
        <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
         <img src={show.moive.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
-        <div className='relative flex flex-col gap-3'></div>
+        <div className='relative flex flex-col gap-3'>
+          <BlurCircle top=" -100px" left="-100px"/>
+          <p className='text-primary'>ENGLISH</p>
+          <h1 className='text-4xl font-semibold max-w-69 text-blance'>{show.movie.title}</h1>
+          <div className='flex items-cener gap-2 text-gray-300'>
+            <StarIcon className='w-5 h-5 text-primary fill-primary'/>
+            {show.movie.vote_average.toFixed(1)}Uaer Rathing
+          </div>
+          <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{
+            show.movie.overview}
+            </p>
+            <p >{timeFormat(show.moive.runtime)}</p>
+        </div>
        </div>
     </div>
   ): <div>Losding...</div>
