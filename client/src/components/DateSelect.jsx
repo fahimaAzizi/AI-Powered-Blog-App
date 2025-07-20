6af5014e1247867e60944f4ef6dfcc472038ca2e
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-20 22:09:05 +0500

    update

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -14,7 +14,16 @@ const DateSelect = ({dateTime ,id}) => {
         </p>
         <div className='flex items-center gap-6 text-sum mt-5'>
           <ChevronLeftIcon width={28} />
-          <span className=''></span>
+          <span className='grid grid-cols-3 md:flex-wrap md:max-w-lg gap-4'>
+            {Object.keys(dateTime).map((date)=>(
+            <button key={date} className='flex flex-col items-center justify-center h-4 w-14 aspect-square rounded cursor-pointer'>
+          
+         
+          <span>{new Date(date).getDate()}</span>
+          <span>{new Date(date).toLocaleDateString("en-US",{month:"short"})}</span>
+            </button>
+            ))} 
+            </span>
         </div>
        </div>
       </div>
