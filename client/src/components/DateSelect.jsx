650a5b02e419c8a4370ca07cef121f2c014f47de
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-20 15:28:48 +0500

    update

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import BlurCircle from './BlurCircle'
 const DateSelect = ({dateTime ,id}) => {
   return (
-    <div id='dateStelect' className='pt-30'></div>
+    <div id='dateStelect' className='pt-30'>
+      <div className='flex flex-col md:flex-row items-center justify-berween gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg'>
+      <BlurCircle />
+
+      </div>
+  
+    </div>
   )
 }
 
