245a2cd703d24033b728310f4573652788b2ca0f
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-26 18:30:31 +0500

    update

--- client/src/pages/seatLayout.jsx
+++ client/src/pages/seatLayout.jsx
@@ -9,13 +9,33 @@ const seatLayout = () => {
   
   const navigate =useNavigate
   const getShow = async() =>{
-
+   const show = dummyShowsData.find(show => show._id === id)
+   if(show){
+    setShow({
+    movie: show,
+    dateTime : dummyDteTimeData
+  })
+   }
   }
   
-  return (
-    <div>
+  return show ? (
+    <div className='flex  flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
+      {/* {available Timings} */}
+      <div className='w-60 bg-primary/10 border boder-primary/20 rounded-lg py-10
+      h-max md:sticky md:top-30'>
+        <p className='text-lg font-semibold px-6'>
+         Available Timings
+        </p>
+        {show.dateTime[date].mp((item)=>
+        <div>
+          <ClockIcon/>
+        </div>
+        )}
+      </div>
 
     </div>
+  ):(
+    <Loading />
   )
 }
 
