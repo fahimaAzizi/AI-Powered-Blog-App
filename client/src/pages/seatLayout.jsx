5e7fbb0ab2a26cc5be158ff7b0e304775b13d379
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-26 18:16:55 +0500

    update

--- client/src/pages/seatLayout.jsx
+++ client/src/pages/seatLayout.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const seatLayout = () => {
+  const {id,date} =useParams
+  const[selectedSeats,setSelectedSeats] =useState([])
+  const[selectedTime,setSelectedTime] =useState([null])
+  const [show , setShow] = useState(null)
+  
+  const navigate =useNavigate
+  const getShow = async() =>{
+
+  }
+  
   return (
-    <div>seatLayout</div>
+    <div>
+
+    </div>
   )
 }
 
