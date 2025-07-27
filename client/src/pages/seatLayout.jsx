ab3fc564221ad87b4038425ae338aecc17b2f623
Fahima Azizi <fahimaazizi397@gmail.com>
2025-07-27 22:03:55 +0500

    update

--- client/src/pages/seatLayout.jsx
+++ client/src/pages/seatLayout.jsx
@@ -26,14 +26,18 @@ const seatLayout = () => {
         <p className='text-lg font-semibold px-6'>
          Available Timings
         </p>
-        {show.dateTime[date].mp((item)=>
         <div>
-          <ClockIcon/>
+        {show.dateTime[date].mp((item)=>
+        <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-rmd cursor-pointer transition ${selectedTime?.time === item.time ?
+          "bg-primary text-white":"hover:bg-primary/20"}`}>
+        
+          <ClockIcon className="w-4 h-4"/>
+          <p className='text-sm'  >{item.time}</p>
         </div>
         )}
-      </div>
-
-    </div>
+        </div>
+        <div/>
+     </div>
   ):(
     <Loading />
   )
