5e283116a03c103847f2bcf6922b8f764c4a1a90
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-03 14:11:32 +0500

    update

--- client/src/pages/seatLayout.jsx
+++ client/src/pages/seatLayout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const SeatLayout = () => {
+  const{id ,date} = useParams()
+  const [selectedSeats , setSelectedSeats] =use
+
   return (
-    <div>SeatLayout</div>
+    <div>
+
+    </div>
   )
 }
 
