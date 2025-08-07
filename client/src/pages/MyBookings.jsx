f9529085930ff7d031354f470e887d41a6463940
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-07 21:09:43 +0500

    update

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react'
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
   const [bookings ,setBooking] =useState([])
-  const
+  const [isLoading ,setIsLoading] =useState([true])
+  
+
+  const getMyBookings = async
 
   return (
     <div>
