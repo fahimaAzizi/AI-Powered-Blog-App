bb90fe0c8b57717972b90e32d164e969ef70d0b2
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-08 23:19:26 +0500

    update

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -18,7 +18,15 @@ const MyBookings = () => {
   return !isLoading (
     <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h[80vh]'>
       <BlurCircle top="100px" left="100px" />
-      
+      <div>
+        <BlurCircle bottom ="0px" left="600px"/>
+      </div>
+      <h1 className='text-lg font-semibold md-4'> My Bookings</h1>
+      {
+        
+      }
     </div>
-  );
-};
+  ): <Loading/>
+}
+
+export default MyBookings
