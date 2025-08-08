61ee49f4c90e7ba7fa45f0701cc7144285b5aa20
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-08 22:50:51 +0500

    update

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dummyBookingData } from '../assets/assets';
 
 const MyBookings = () => {
@@ -11,10 +11,14 @@ const MyBookings = () => {
     setBookings(dummyBookingData);
     setIsLoading(false);
   };
+  useEffect(()=>{
+    getMyBookings()
+  },[])
 
-  return (
-    <div>
-      {/* JSX output here */}
+  return !isLoading (
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h[80vh]'>
+      <BlurCircle top="100px" left="100px" />
+      
     </div>
   );
 };
