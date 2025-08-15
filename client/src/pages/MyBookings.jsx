2b22a10afaaeefd230001471521eb31d504504d7
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-15 17:11:45 +0500

    update

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../assets/assets'
 import Loading from '../components/Loading'
+import BlurCircle from '../components/BlurCircle'
 
 const MyBookings = () => {
   const currency = import.meta.env_VITE_CURRENCY
@@ -16,9 +17,15 @@ const MyBookings = () => {
   },[])
 
   return !isLoading ?(
-    <div>
-       <h1>sdfuiho</h1>
-    </div>
+     <div className="relative px-6 md:px-16 lg:px-40 pt-10 md:pt-20 min-h-[80vh]">
+      <BlurCircle top="100px" left="100px"/>
+      <div>
+        <BlurCircle/>
+      </div>
+      <h1>
+        MyBookings
+      </h1>
+     </div>
  
  
   ): <Loading/>
