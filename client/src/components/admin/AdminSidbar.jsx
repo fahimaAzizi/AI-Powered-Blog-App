b571463c18793b1fce3941bfea38789bc2fd3047
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-20 22:37:57 +0500

    update

---
+++ client/src/components/admin/AdminSidbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const AdminSidbar=()=>{
+  const user ={
+    firstName: 'admin',
+    lastName :'user',
+    imageUrl: assets.profile,
+  }
+}
+
+
+
+
+
+
+function AdminSidbar() {
+  return (
+    <div>AdminSidbar</div>
+  )
+}
+
+export default AdminSidbar
