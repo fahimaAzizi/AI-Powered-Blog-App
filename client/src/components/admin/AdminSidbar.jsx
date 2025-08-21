ed0339db354934dd54c19d9ca5ef0149c6240029
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-21 22:54:19 +0500

    update

--- client/src/components/admin/AdminSidbar.jsx
+++ client/src/components/admin/AdminSidbar.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
+import { LayoutDashboardIcon, PlusSquareIcon, ListIcon, ListCollapseIcon } from 'lucide-react'
+import assets from '../assets/assets' // adjust path if needed
 
-const AdminSidbar=()=>{
-  const user ={
-    firstName: 'admin',
-    lastName :'user',
+const AdminSidebar = () => {
+  const user = {
+    firstName: 'Admin',
+    lastName: 'User',
     imageUrl: assets.profile,
   }
-}
 
-
-
-
-
-
-function AdminSidbar() {
-  return (
-    <div>AdminSidbar</div>
-  )
-}
-
-export default AdminSidbar
+  const adminNavlinks = [
+    { name: 'Dashboard', path: '/admin', icon: LayoutDashboardIcon },
+    { name: 'Add Shows', path: '/admin/add-shows', icon: PlusSquareIcon },
+    { name: 'List Shows', path: '/admin/list-shows', icon: ListIcon },
+    { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon },
+  ]
