6b3f894a4295e58f407e0f054fa963acd30b8345
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-22 23:24:28 +0500

    update

---
+++ client/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from "lucide-react";
+
+const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  const dashboardCards = [
+    {
+      title: "Total Bookings",
+      value: dashboardData.totalBookings || "0",
+      icon: ChartLineIcon,
+    },
