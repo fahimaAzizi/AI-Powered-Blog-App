650332b51e75ca952cdf8097168d63d5315d45e5
Fahima Azizi <fahimaazizi397@gmail.com>
2025-08-26 22:26:26 +0500

    update

--- client/src/components/admin/AdminSidbar.jsx
+++ client/src/components/admin/AdminSidbar.jsx
@@ -31,6 +31,36 @@ const AdminSidebar = () => {
       />
       <p className="mt-2 text-base max-md:hidden">
         {user.firstName} {user.lastName}
-      
+      </p>
+
+      {/* Navigation Links */}
+      <div className="w-full mt-6">
+        {adminNavLinks.map((link, index) => (
+          <NavLink
+            key={index}
+            to={link.path}
+            className={({ isActive }) =>
+              `relative flex items-center gap-2 w-full py-2.5 px-4 md:pl-10 text-gray-400 hover:text-primary transition 
+              ${isActive ? "bg-primary/15 text-primary font-semibold" : ""}`
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <link.icon className="w-5 h-5" />
+                <p className="max-md:hidden">{link.name}</p>
+                {/* Active Indicator */}
+                <span
+                  className={`w-1 h-10 rounded-l right-0 absolute ${
+                    isActive ? "bg-primary" : ""
+                  }`}
+                ></span>
+              </>
+            )}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AdminSidebar;
