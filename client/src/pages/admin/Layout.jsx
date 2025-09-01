098c56abeca9fc4c8e44a05b66edbf52549477b5
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-01 23:00:36 +0500

    update

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
 import AdminSidbar from '../../components/admin/AdminSidbar'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
     <AdminNavbar/>
-    <div>
+    <div className='flex'>
         <AdminSidbar/>
+        <div  className='flex-1 px-4 py-10 h-[calc(100vh-64px)] overflow-y-auto'>
+          <Outlet />
+        </div>
     </div>
     </>
   )
