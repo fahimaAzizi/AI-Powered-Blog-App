93f931dea1e086eeeca59b99a911bfa2f116cf92
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-01 22:48:47 +0500

    update

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -54,9 +54,23 @@ const Dashboard = () => {
   return !loading ?(
     <>
       <Title text1="Admin" text2="Dashboerd"/>
-      <div>
-        
+      <div className="relative flex flex-wrap gap-4 mt-6">
+  <BlurCircle top="-100px" left="0" />
+  <div className="flex flex-wrap gap-4 w-full">
+    {dashboardCards.map((card, index) => (
+      <div
+        key={index}
+        className="flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full"
+      >
+        <div>
+          <h1 className="text-sm">{card.title}</h1>
+          <p className="text-xl font-medium mt-1">{card.value}</p>
+        </div>
+        <card.icon className="w-6 h-6" />
       </div>
+    ))}
+  </div>
+</div>
     </>
   ) : <Loading/>
 
