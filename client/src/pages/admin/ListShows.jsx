c1d82ae0b5081496ebada95e5571e6338caaabc8
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-04 19:35:50 +0500

    update

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ListShows() {
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [shows ,setShows] = useState([]); 
+  cost [loading ,setLoadsing] = useState(true);
+
+  const getAllShows = async () =>{
+    
+  }
+ 
   return (
-    <div>ListShows</div>
+    <div>
+      
+    </div>
   )
 }
 
