1d6c6cf995dfe57ab621919ff110cda9c2884d14
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-05 19:56:05 +0500

    Update

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -12,9 +12,24 @@ function ListShows() {
  
   return (
     <div>
-      
+      <tbody className="text-sm font-light">
+  {shows.map((show, index) => (
+    <tr
+      key={index}
+      className="border-b border-primary/10 odd:bg-primary/5 even:bg-primary/10"
+    >
+      <td className="p-2 min-w-45 pl-5">{show.movie.title}</td>
+      <td className="p-2">{dateFormat(show.showDateTime)}</td>
+      <td className="p-2">{Object.keys(show.occupiedSeats).length}</td>
+      <td className="p-2">
+        {currency} {Object.keys(show.occupiedSeats).length * show.showPrice}
+      </td>
+    </tr>
+  ))}
+</tbody>
+
     </div>
-  )
+  ): <Loading/>
 }
 
 export default ListShows 
