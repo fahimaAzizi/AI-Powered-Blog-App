1b3fdc23eae74a474a6abdde456f5c50411dcb36
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-07 20:04:23 +0500

    update

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { cache, useEffect, useState } from "react";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import { currency } from "../../App"; 
 import dateFormat from "../../utils/dateFormat"; // make sure this file exists!
+import { dummyShowsData } from "../../assets/assets";
 
 const ListShows = () => {
   const [shows, setShows] = useState([]);
@@ -12,13 +13,23 @@ const ListShows = () => {
   try{
 
     setShows([{
-      
-    }])
+      movie : dummyShowsData[0],
+      showDateTime :"2025-06-30T02:30:00.000Z",
+      showPrice :59,
+      occupiedSeats:{
+        A1:"uaer_1",
+        B1:"uaer_2",
+        C1:"uaer_3"
+      }
+    }]);
+    setLoading(false);
+  } catch(error){
+      console.error(error)
   }
  }
 
   useEffect(() => {
-    fetchShows();
+    getAllShows();
   }, []);
 
   return !loading ? (
