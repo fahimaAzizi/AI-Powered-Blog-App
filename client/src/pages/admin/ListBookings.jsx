e79bf1259705a6e30535b273ba5c81ca68db301c
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-11 21:18:16 +0500

    update

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -20,7 +20,15 @@ const ListBookings = () => {
   return !isLoading?(
     <>
     <Title text1 ="list" text2="Bookings" />
-         
+         <thead>
+            <tr className="bg-primary/20 text-left text-white">
+              <th className="p-2 font-medium pl-5">User Name</th>
+              <th className="p-2 font-medium">Movie Name</th>
+              <th className="p-2 font-medium">Show Time</th>
+              <th className="p-2 font-medium">Seats</th>
+              <th className="p-2 font-medium">Amount</th>
+            </tr>
+          </thead>
     </>
   ): <Loading/>
 }
