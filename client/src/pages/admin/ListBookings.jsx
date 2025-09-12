8f754bfe5d214a5c2d264b480d4c5e6f4ac3e9f1
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-12 11:34:07 +0500

    update

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -20,6 +20,8 @@ const ListBookings = () => {
   return !isLoading?(
     <>
     <Title text1 ="list" text2="Bookings" />
+    <div className='max-w-4xl mt-6 overflow-x-auto'>
+      <table className='w-full border-collapse rounded-md overflow-hidden text-norap'>
          <thead>
             <tr className="bg-primary/20 text-left text-white">
               <th className="p-2 font-medium pl-5">User Name</th>
@@ -29,6 +31,28 @@ const ListBookings = () => {
               <th className="p-2 font-medium">Amount</th>
             </tr>
           </thead>
+                    <tbody className="text-sm font-light">
+            {bookings.map((item, index) => (
+              <tr
+                key={index}
+                className="border-b border-primary/20 bg-primary/5 even:bg-primary/10"
+              >
+                <td className="p-2 min-w-45 pl-5">{item.user.name}</td>
+                <td className="p-2">{item.show.movie.title}</td>
+                <td className="p-2">{dateFormat(item.show.showDateTime)}</td>
+                <td className="p-2">
+                  {Object.keys(item.bookedSeats)
+                    .map((seat) => item.bookedSeats[seat])
+                    .join(", ")}
+                </td>
+                <td className="p-2">
+                  {currency} {item.amount}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          </table>
+      </div>
     </>
   ): <Loading/>
 }
