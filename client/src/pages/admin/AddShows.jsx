974b7b7bd31792879dafd5824ebba824953f9948
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-14 16:49:37 +0500

    update

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -17,8 +17,16 @@ const AddShows =()=>{
     fetchNowplayingMovies();
   },[]);
 
-  return (
-    <div></div>
+  return nowPlayingMovies.length > 0? (
+    <>
+    <title text1="add" text2="Show"/>
+    <p className='mt-10 text=lg font-medium'> Now Playing Movies</p>
+    <div className='overflow-x-auto pb-4'>
+      <div className='group flex flrx-wrap gap-4 mt-4 w-max'>
+
+      </div>
+    </div>
+    </>
   )
 }
 
