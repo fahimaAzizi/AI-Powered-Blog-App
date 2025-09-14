fac9a23cc36a39497457c55f5891eba66b4ccbee
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-14 16:43:30 +0500

    update

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,36 +1,25 @@
-import React from 'react'
-import Title from '../../components/admin/Title';
+import React, { useEffect, useState } from 'react'
 
-function AddShow() {
+
+
+const AddShows =()=>{
+  const currency = import.meta.env.VITE_CURRENCY
+  cosnt [nowPlayingMovies , setNowPlayingMovies] = useState([]);
+  cosnt [selecedMovies , setSelecedMovies] = useState(null);
+  cosnt [dateTimeSlelection , setDateTimeSlelection] = useState({});
+  cosnt [dateTimeInput , setDateTimeInput] = useState("");
+  cosnt [showPrice  , setShowPrice] = useState("");
+
+  const fetchNowplayingMovies =async ()=>{
+    setNowPlayingMovies(dummyShowsData)
+  };
+  useEffect(() =>{
+    fetchNowplayingMovies();
+  },[]);
+
   return (
-    <div>
-       <Title text1="Add" text2="Shows" />
-      <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
-
-      <div className="overflow-x-auto pb-4">
-        <div className="group flex flex-wrap gap-4 mt-4 w-max">
-          {nowPlayingMovies.map((movie) => (
-            <div
-              key={movie.id}
-              className="relative max-w-40 cursor-pointer 
-                         group-hover:not-hover:opacity-40 
-                         hover:-translate-y-1 
-                         transition duration-300"
-            >
-              {/* Movie content goes here */}
-              <img
-                src={movie.poster_path} className='w-full'/>
-               
-               
-              
-              <p className="mt-2 text-sm text-center">{movie.title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-   
+    <div></div>
   )
 }
 
-export default AddShow
+export default AddShows
