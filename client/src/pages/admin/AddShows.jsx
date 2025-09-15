9d91b75f9d20e0c2ca999c7b2d85a8b57707d002
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-15 19:47:40 +0500

    update

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '../../components/Loading';
+import { dummyShowsData } from '../../assets/assets';
 
 
 
-const AddShows =()=>{
-  const currency = import.meta.env.VITE_CURRENCY
-  cosnt [nowPlayingMovies , setNowPlayingMovies] = useState([]);
-  cosnt [selecedMovies , setSelecedMovies] = useState(null);
-  cosnt [dateTimeSlelection , setDateTimeSlelection] = useState({});
-  cosnt [dateTimeInput , setDateTimeInput] = useState("");
-  cosnt [showPrice  , setShowPrice] = useState("");
+const AddShows = () =>{
+ const currency = import.meta.env.VITE_CURRENCY;
+
+
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("");
+  const [showPrice, setShowPrice] = useState("");
 
   const fetchNowplayingMovies =async ()=>{
     setNowPlayingMovies(dummyShowsData)
@@ -21,10 +24,14 @@ const AddShows =()=>{
   return nowPlayingMovies.length > 0? (
     <>
     <title text1="add" text2="Show"/>
-    <p className='mt-10 text=lg font-medium'> Now Playing Movies</p>
+    <p className='mt-10 text-lg font-medium'> Now Playing Movies</p>
     <div className='overflow-x-auto pb-4'>
       <div className='group flex flrx-wrap gap-4 mt-4 w-max'>
-
+        {nowPlayingMovies.map((movie)=>(
+         <div>
+           
+         </div>
+      ))}
       </div>
     </div>
     </>
