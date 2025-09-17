ea2512ae2b691395dcba958616060ddd9325ba51
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-17 22:39:24 +0500

    update

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -33,7 +33,9 @@ const AddShows = () =>{
            <div className='relative rounded-lg overflow-hidden'>
             <img src={movie.poster-path} className='w-full object-cover brightness-90' alt="" />
             <div className='text-sm flex item-center'>
+          <p className='flex item-center gap-1'>
 
+          </p>
             </div>
             </div> 
          </div>
