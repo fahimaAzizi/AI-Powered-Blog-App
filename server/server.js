4b70f5562f0a716f170c30afc18bcdc7c5ec98de
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-23 22:22:26 +0500

    update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+
+const app =express();
+
+const port = 3000 ;
+
+app.use(express.json())
+app.use(cors())
+
+app.getMaxListeners('/')
