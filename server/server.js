e952dc31ba8ed8ffc16e0552aba10bc9e94c5a7c
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-24 00:02:21 +0500

    update

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 
-const app =express();
+const app = express();
 
-const port = 3000 ;
+const port = 3000;
 
 app.use(express.json())
 app.use(cors())
 
-app.getMaxListeners('/')
+app.get('/', (req,res)=>res.send('Server is Live!'))
+
+app.listen(port ,()=> console.log(`server listening at http://localhost:${port}`));
