c56ad50d88540066dc27b867d96298ee191be5e3
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-27 19:49:25 +0500

    update

---
+++ server/Inngest/index.js
@@ -0,0 +1,5 @@
+import { Inngest } from "inngest";
+export const inngest = new Inngest({ id: "QuikBlog" });
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [];
