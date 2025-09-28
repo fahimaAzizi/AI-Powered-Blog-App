ca5976e189f35f2a35446d36c1ddd1326af31908
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-28 19:38:04 +0500

    update

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -8,4 +8,18 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    cons
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0]?.email_address || "",
+      name: `${first_name} ${last_name}`,
+      image: image_url || "",
+    };
+
+    // TODO: Save userData to your database
+    console.log("User Data Synced:", userData);
+
+    return { success: true, userData };
+  }
+);
