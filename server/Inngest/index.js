06a313219fae0750df85541818dd7c55bcaab8e6
Fahima Azizi <fahimaazizi397@gmail.com>
2025-09-28 19:37:39 +0500

    update

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -1,5 +1,11 @@
 import { Inngest } from "inngest";
-export const inngest = new Inngest({ id: "QuikBlog" });
 
-// Create an empty array where we'll export future Inngest functions
-export const functions = [];
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    cons
