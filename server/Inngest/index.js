17f12b391c6203844c4776a35348c3146171a263
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-02 23:07:06 +0500

    update

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-// import User from "../models/User"; // <- Uncomment and adjust path to your User model
+import User from "../models/user"
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -42,7 +42,12 @@ const syncUserUpdation = inngest.createFunction(
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
-    
-
+    const userData ={
+      _id: id,
+      email: email_addresses[0].email_address,
+      name:first_name+' '+last_name,
+      image:image_url
+    }
+     await User.create(userData)
   }
 );
