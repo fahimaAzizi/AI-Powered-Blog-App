7908b8b2f630c2d6231d3063500e387c6b9fc781
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-03 22:03:38 +0500

    update

--- server/models/movie.js
+++ server/models/movie.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema(
     {
-        
+        _id :{ type: String ,required: true},
+        title :{ type: String ,required: true},
+        overview:{ type: String ,required: true},
+        poster_path :{ type: String ,required: true},
+        backdrop_path:{ type: String ,required: true},
+        release_date :{ type: String ,required: true},
+        original_language:{ type: String },
+        tagline:{ type: String },
+        genres:{ type: String },
     }
 )
