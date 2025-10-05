e27319fa453542b39712ff4ff6c80627357545dd
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-05 23:08:13 +0500

    update

--- server/conotrollers/showController.js
+++ server/conotrollers/showController.js
@@ -1,8 +1,12 @@
+
+
+
 export const getNowPlayingMovies = async(req , res) =>{
     try{
+       await axios.get('https://api.themoviedb.org/3/movie/now_playing')
 
     }catch (error ){
-        
+
     }
   
 }
