126d74849b7d4f70a2f4b825f7f6a37b07cbaf09
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-05 23:13:50 +0500

    update

--- server/conotrollers/showController.js
+++ server/conotrollers/showController.js
@@ -3,10 +3,16 @@
 
 export const getNowPlayingMovies = async(req , res) =>{
     try{
-       await axios.get('https://api.themoviedb.org/3/movie/now_playing')
+       await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
+        Headers: {Authorization: `bearer ${process.env.TMDB_API_KEY}`}
+       })
+
+       const movies =DataTransfer.results;
+       res.json({success :true ,movies: movies})
 
     }catch (error ){
-
+      console.error(error);
+      res.json({success:false, message:error.message})
     }
   
 }
