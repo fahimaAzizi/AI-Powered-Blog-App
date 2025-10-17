beec9f6f8c37b04cf09560b920fe4b3c36e613cc
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-17 19:55:11 +0500

    update

--- server/controllers/showRoutes.js
+++ server/controllers/showRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { addShow, getNowPlayingMovies } from "./showController";
+import { addShow, getNowPlayingMovies, getShow } from "./showController";
 
 
 const showRoute = express.Router();
+
 showRoute.get('/now-playing', getNowPlayingMovies)
 showRoute.post('/add',addShow)   
+showRoute.get("all", getShow)
+showRoute.get("/:movieId", getShow)
 
 export default showRoute;
