d74b0694a1ebcb329ff04f1ed33bb5700064c7fe
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-19 18:44:47 +0500

    update

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -4,5 +4,13 @@ import Show from "../models/Show.js"
 const checkSeatsAvailability =async (showId,selectedSeats)=>{
  try{
     const showData = await showId.findById(showId)
+    if(!showData) return false;
+
+    const occupiedSeats =showData.occupiedSeats;
+    const isAnySeatTaken =selectedSeats.some(seat => occupiedSeats[seat])
+ }catch(error){
+
  }
+
+  
 }
