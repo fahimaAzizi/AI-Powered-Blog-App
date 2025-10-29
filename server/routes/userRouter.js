0cffc08c265ce2adc3371abd97c4bcbae37c3932
Fahima Azizi <fahimaazizi397@gmail.com>
2025-10-29 15:39:53 +0500

    update

---
+++ server/routes/userRouter.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getFavorites, getUserBookings,updateFavorite } from '../controllers/userController';
+
+
+const userRouter = express.Router();
+
+userRouter.get('/bookings', getUserBookings)
+userRouter.post('/update-favorite', updateFavorite)
+userRouter.get('/favorite', getFavorites)
+
+export default userRouter;
