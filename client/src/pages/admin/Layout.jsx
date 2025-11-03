f39ae09812f9796847f6740b7a7df5739d351810
Fahima Azizi <fahimaazizi397@gmail.com>
2025-11-03 22:53:30 +0500

    update

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect } from 'react'
-import AdminNavbar from '../../components/admin/AdminNavbar'
-import AdminSidbar from '../../components/admin/AdminSidbar'
-import { Outlet } from 'react-router-dom'
-import Loading from '../../components/Loading'
-import { useAppContext } from '../../context/AppContext'
+import React, { useEffect, useState } from 'react';
+import AdminNavbar from '../../components/admin/AdminNavbar';
+import AdminSidbar from '../../components/admin/AdminSidbar';
+import { Outlet } from 'react-router-dom';
+import Loading from '../../components/Loading';
+import { useAppContext } from '../../context/AppContext';
 
-const Layout=()=> {
-  const {isAdmin, fetchIsAdmin} = useAppContext()
+const Layout = () => {
+  const { isAdmin, fetchIsAdmin } = useAppContext();
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    fetchIsAdmin()
-  },[])
+  useEffect(() => {
+    const checkAdmin = async () => {
+      await fetchIsAdmin();
+      setLoading(false);
+    };
+    checkAdmin();
+  }, []);
+
+  if (loading) return <Loading />;
+
   return (
     <>
-    <AdminNavbar/>
-    <div className='flex'>
-        <AdminSidbar/>
-        <div  className='flex-1 px-4 py-10 h-[calc(100vh-64px)] overflow-y-auto'>
+      <AdminNavbar />
+      <div className="flex">
+        <AdminSidbar />
+        <div className="flex-1 px-4 py-10 h-[calc(100vh-64px)] overflow-y-auto">
           <Outlet />
         </div>
-    </div>
+      </div>
     </>
-  ) : <Loading/>
-}
+  );
+};
 
-export default Layout
+export default Layout;
