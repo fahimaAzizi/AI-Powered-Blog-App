9035c7053d6eb8005e65215786057506f9ed55db
Fahima Azizi <fahimaazizi397@gmail.com>
2025-11-07 17:54:01 +0500

    update

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { dummyShowsData } from '../assets/assets'
 import MovieCard from '../components/MovieCard'
 import BlurCircle from '../components/BlurCircle'
+import { useAppContext } from '../context/AppContext'
 
 const Favorite = () => {
+  const {FavoriteMovies} = useAppContext
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <BlurCircle top="150px" left="0px" />
