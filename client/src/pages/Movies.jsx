8d80a9e9ca8724b10eeee41b1fd59b928968b37e
Fahima Azizi <fahimaazizi397@gmail.com>
2025-11-28 20:44:13 +0500

    Initial commit

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -4,24 +4,29 @@ import BlurCircle from '../components/BlurCircle'
 import { useAppContext } from '../context/AppContext'
 
 const Movies = () => {
+  const { shows } = useAppContext();
 
-  const { shows } = useAppContext()
-  return shows.length > 0 ? (
-    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+  // Safe fallback if "shows" is undefined/null
+  const movieList = Array.isArray(shows) ? shows : [];
+
+  return movieList.length > 0 ? (
+    <div className="relative my-32 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
       <BlurCircle top="150px" left="0px" />
       <BlurCircle bottom="50px" right="50px" />
 
-      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <h1 className="text-xl font-semibold my-6">Now Showing</h1>
 
-      <div className='flex flex-wrap max-sm:justify-center gap-8'>
-        {shows.map((movie) => (
-          <MovieCard movie={movie} key={movie._id} />
+      <div className="flex flex-wrap gap-8 sm:justify-start justify-center">
+        {movieList.map((movie, index) => (
+          <MovieCard movie={movie} key={movie?._id || index} />
         ))}
       </div>
     </div>
   ) : (
-    <p className="text-center text-gray-400">No movies to show.</p>
-  )
-}
+    <div className="min-h-[70vh] flex items-center justify-center">
+      <p className="text-gray-400 text-lg">No movies to show.</p>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
